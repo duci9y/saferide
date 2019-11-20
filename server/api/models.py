7816533d1418ea43a252b9df1ea0dc39269241a0
yojiwatanabe <yojiwata@gmail.com>
2019-11-19 21:20:58 -0500

    added created_on and updated_on fields for users and rides

--- server/api/models.py
+++ server/api/models.py
@@ -4,13 +4,17 @@ from django.contrib.gis.db import models
 class User(models.Model):
     uuid        = models.CharField(max_length=60)
     abuse_lock  = models.BooleanField(default=False)
+    datetime_created    = models.DateTimeField(auto_now_add=True)
+    datetime_updated    = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.uuid
 
 class Ride(models.Model):
-    start_loc = models.PointField(null=True)
-    end_loc   = models.PointField(null=True)
-    rider_uid = models.ForeignKey(
-            'auth.User', on_delete=models.CASCADE,
+    start_loc           = models.PointField(null=True)
+    end_loc             = models.PointField(null=True)
+    datetime_created    = models.DateTimeField(auto_now_add=True)
+    datetime_updated    = models.DateTimeField(auto_now=True)
+    rider_uid           = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE,
     )
