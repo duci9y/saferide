a2961ab595e611cad2b29292370178216524f139
yojiwatanabe <yojiwata@gmail.com>
2019-11-19 21:46:44 -0500

    setting postgis engine as default manually

--- server/saferide/settings.py
+++ server/saferide/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 DATABASES = {}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 django_heroku.settings(locals())
