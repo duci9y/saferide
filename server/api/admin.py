c7315a50cf89eca4ef6581704b9f1a34e6c2326e
yojiwatanabe <yojiwata@gmail.com>
2019-11-23 23:34:21 -0500

    added drivers table

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ride, User
+from .models import Ride, User, Driver
 
 # Register your models here.
 admin.site.register(Ride)
 admin.site.register(User)
+admin.site.register(Driver)
