aa0332f9ec64d4e505861acc2045b5c86c0ec1e6
yojiwatanabe <yojiwata@gmail.com>
2019-11-23 23:33:58 -0500

    updated rides and users table, added drivers table

--- server/api/models.py
+++ server/api/models.py
@@ -1,20 +1,52 @@
 from django.contrib.gis.db import models
+from django.contrib.auth.models import User as dj_user
 
-# Create your models here.
+# Users Table
 class User(models.Model):
-    uuid        = models.CharField(max_length=60)
-    abuse_lock  = models.BooleanField(default=False)
+    uuid                = models.CharField(max_length=60)
     datetime_created    = models.DateTimeField(auto_now_add=True)
     datetime_updated    = models.DateTimeField(auto_now=True)
+    abuse_lock          = models.BooleanField(default=False)
+    active              = models.BooleanField(default=True)
 
     def __str__(self):
         return self.uuid
 
+
+# Drivers Table
+class Driver(models.Model):
+    user                = models.OneToOneField(dj_user,
+                                               on_delete=models.CASCADE)
+    current_loc         = models.PointField(null=True)
+    on_duty             = models.BooleanField(default=False)
+    datetime_created    = models.DateTimeField(auto_now_add=True)
+    datetime_updated    = models.DateTimeField(auto_now=True)
+    active              = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.first_name
+
+
+# Rides Table
 class Ride(models.Model):
     start_loc           = models.PointField(null=True)
     end_loc             = models.PointField(null=True)
     datetime_created    = models.DateTimeField(auto_now_add=True)
     datetime_updated    = models.DateTimeField(auto_now=True)
-    rider_uid           = models.ForeignKey(
-        'auth.User', on_delete=models.CASCADE,
-    )
+    datetime_ended      = models.DateTimeField(null=True)
+    active              = models.BooleanField(default=True)
+    cofirmed            = models.BooleanField(default=False)
+    completed           = models.BooleanField(default=False)
+    abuse_locked        = models.BooleanField(default=False)
+    rider               = models.ForeignKey(User,
+                                            on_delete=models.CASCADE,
+                                            null=True)
+    driver              = models.ForeignKey(Driver,
+                                            on_delete=models.CASCADE,
+                                            null=True)
+
+    def __str__(self):
+        if self.abuse_locked:
+            return "Locked ride at " % self.datetime_created
+
+        return "Ride at %s" % self.datetime_created
