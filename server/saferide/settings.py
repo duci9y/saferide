934b1698808acc29d00bcc670651abae6f061fff
yojiwatanabe <yojiwata@gmail.com>
2019-11-24 21:13:28 -0500

    added rest framework to installed apps, added cap of 10 results per page

--- server/saferide/settings.py
+++ server/saferide/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 if DATABASES.get('default') and DATABASES['default'].get('OPTIONS'):
     del DATABASES['default']['OPTIONS']['sslmode']
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
