4e792b5eaa432770960f154698cda66e7b398419
yojiwatanabe <yojiwata@gmail.com>
2019-11-24 21:11:45 -0500

    created viewsets for db tables

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer, DriverSerializer, RideSerializer
+from .models import User, Driver, Ride
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('uuid')
+    serializer_class = UserSerializer
+
+
+class DriverViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all().order_by('on_duty')
+    serializer_class = DriverSerializer
+
+
+class RideViewSet(viewsets.ModelViewSet):
+    queryset = Ride.objects.all().order_by('active')
+    serializer_class = RideSerializer
