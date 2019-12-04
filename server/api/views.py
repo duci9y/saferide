178c5321d883ba7b2c8f3317177dad7388b94117
yojiwatanabe <yojiwata@gmail.com>
2019-12-04 15:09:44 -0500

    added abuse_lock logic

--- server/api/views.py
+++ server/api/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from json import dumps
+import json
 
 from .serializers import UserSerializer, DriverSerializer, RideSerializer
 from .models import User, Driver, Ride
@@ -15,20 +15,21 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     @action(detail=True, methods=['post', 'get'], name='Create a new ride')
-    def create_ride(self, request, pk=None):
+    def create_ride(cls, request, pk=None):
+        # NOTE: request.data returns a query dictionary, not regular dict
         driver_obj = Driver.objects.all().first()
         serializer = DriverSerializer(driver_obj, context={'request': request})
 
         user_obj = User.objects.get(pk=pk)
         serializer = UserSerializer(user_obj, context={'request': request})
 
-        # if (user_obj.abuse_lock):
-        #     print('Cannot provide a ride to user (abuse_lock active)')
-        # else:
-            # add driver and user to ride-entry
-            # push new entry to ride-Table
+        if (user_obj.abuse_lock):
+            return Response('Ride denied - user is abuse locked', status=status.HTTP_403_FORBIDDEN)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        request_data = request.data.__getitem__('abuse_lock')
+
+        return Response(request_data, status=status.HTTP_200_OK)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
 
 class DriverViewSet(viewsets.ModelViewSet):
     queryset = Driver.objects.all().order_by('on_duty')
